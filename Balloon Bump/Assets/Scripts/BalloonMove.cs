2220819c60df9711ba29e2cec66521077ca1b41f
Curtis <curtisfedorko203@gmail.com>
2022-03-29 23:47:31 -0400

    better balloon movement and tests for the background

--- Balloon Bump/Assets/Scripts/BalloonMove.cs
+++ Balloon Bump/Assets/Scripts/BalloonMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BalloonMove : MonoBehaviour
 {
-    public float speed = 50f;
+    public float speed = 10f;
 public static bool isHit; 
 public int tracky; 
 void Start(){
@@ -13,16 +13,17 @@ void Start(){
 }
 void Update()
 {
-    if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)){
+    // Make balloon float upwards slowly
+    transform.Translate(Vector3.up * 1f * Time.deltaTime); 
+
+    if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
         transform.Translate(Vector3.left * speed * Time.deltaTime); 
     }
-      if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)){
+      if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)){
         transform.Translate(Vector3.down * speed * Time.deltaTime); 
     }
-    if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)){
-        transform.Translate(Vector3.up * speed * Time.deltaTime); 
-    }
-     if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)){
+        
+     if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
         transform.Translate(Vector3.right * speed * Time.deltaTime); 
     }
 }
