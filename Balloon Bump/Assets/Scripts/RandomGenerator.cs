c02a6c88b584964e1abadd4f047c726d79bc15b6
Curtis <curtisfedorko203@gmail.com>
2022-07-01 09:28:02 -0400

    Respawning after extra life fixed, random generator for portal and clouds fixed.

--- Balloon Bump/Assets/Scripts/RandomGenerator.cs
+++ Balloon Bump/Assets/Scripts/RandomGenerator.cs
@@ -8,6 +8,7 @@ public class RandomGenerator : MonoBehaviour
     public float timey = 0f; 
     public float randomtime = 0f; 
     public float darttime = 0f; 
+    public float portaltime = 0f; 
     public bool heart = false; 
     public float totaltime = 0f; 
     public bool tracky = false; 
@@ -17,6 +18,7 @@ public class RandomGenerator : MonoBehaviour
     }
     void Update()
     {
+        portaltime += Time.deltaTime; 
          totaltime += Time.deltaTime; 
          timey += Time.deltaTime; 
          darttime += Time.deltaTime; 
@@ -31,7 +33,11 @@ if(totaltime < 30){
          }
 }
 
-if(totaltime > 30){
+if(totaltime > 30 && totaltime < 60){
+    if(portaltime >= 12){
+        portaltime = 0;
+        SpawnPortal(); 
+    }
          if(timey >= 4){
              timey = 0; 
              SpawnNewCloud(); 
@@ -45,6 +51,52 @@ if(totaltime > 30){
              SpawnDart(); 
          }
 }
+
+if(totaltime > 60 && totaltime < 75){
+   if(portaltime >= 10){
+        portaltime = 0;
+        SpawnPortal(); 
+    }
+         if(timey >= 3.5){
+             timey = 0; 
+             SpawnNewCloud(); 
+         }
+         if(darttime >= 8){
+              darttime = 0; 
+             SpawnDart(); 
+         }
+
+}
+if(totaltime > 75 && totaltime < 100){
+   if(portaltime >= 7){
+        portaltime = 0;
+        SpawnPortal(); 
+    }
+         if(timey >= 3){
+             timey = 0; 
+             SpawnNewCloud(); 
+         }
+         if(darttime >= 5){
+              darttime = 0; 
+             SpawnDart(); 
+         }
+
+}
+if(totaltime > 100){
+   if(portaltime >= 4){
+        portaltime = 0;
+        SpawnPortal(); 
+    }
+         if(timey >= 2){
+             timey = 0; 
+             SpawnNewCloud(); 
+         }
+         if(darttime >= 2){
+              darttime = 0; 
+             SpawnDart(); 
+         }
+
+}
 //if(ExtraLife.has_done && !tracky){
     //SpawnBalloon();
     //tracky = true; 
@@ -56,7 +108,7 @@ if(totaltime > 30){
         GameObject nc = Instantiate(newCloud[0], this.transform) as GameObject; 
         GameObject nc2 = Instantiate(newCloud[0], this.transform) as GameObject; 
         nc.transform.localPosition = new Vector3(Random.Range(-13.9f, 12f), 2.25f, 0f);
-        nc2.transform.localPosition = new Vector3(Random.Range(-13.9f, 12f), 2.25f, 0f);
+        nc2.transform.localPosition = new Vector3(Random.Range(-13.9f, 12f), 1.25f, 0f);
 
     }
     public void SpawnHeart(){
@@ -70,5 +122,9 @@ if(totaltime > 30){
    public void SpawnBalloon(){
         GameObject nc = Instantiate(newCloud[3], this.transform) as GameObject; 
         nc.transform.localPosition = new Vector3(0f , -1.5f, 0f);
+   }
+   public void SpawnPortal(){
+        GameObject nc = Instantiate(newCloud[4], this.transform) as GameObject; 
+        nc.transform.localPosition = new Vector3(Random.Range(-12.75f,12.75f), 10.2f, 0f);
    }
 }
